9a957c3518c59364fa4a5d4eab0042635d9baece
Stephen <stvmwg@gmail.com>
2025-03-19 08:12:39 +0300

    Add files via upload

---
+++ engines/story_content/triggers.js
@@ -0,0 +1,41 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5VdUXv2AezF":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6lcQs2cu4pA');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6YRmIkOnp6O';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
